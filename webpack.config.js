99d5ea9184ebf4152881e12101bdb503b9ebc3e4
Fajar <fajars04@outlook.com>
2023-05-25 08:31:42 +0700

    create blog.html, but there is problem about devserver livereload

--- webpack.config.js
+++ webpack.config.js
@@ -2,39 +2,39 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-
 module.exports = {
-    entry: './src/scripts/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'main.js',
-        clean: true
+  entry: './src/scripts/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
+    clean: true,
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, './dist'),
     },
-    mode: 'development',
-    module: {
-        rules: [
-            {
-                test: /\.s[ac]ss$/i,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    "css-loader",
-                    "sass-loader"
-                ],
-            }
-        ]
-    },
-    devServer: {
-        static: {
-            directory: path.join(__dirname, './src'),
-        },
-        compress: true,
-        port: 3000,
-        liveReload: true
-    },   
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html'
-        }),
-        new MiniCssExtractPlugin()
-    ]
-}
+    compress: true,
+    port: 3000,
+    liveReload: true,
+  },
+  plugins: [
+    // new HtmlWebpackPlugin({
+    //   filename: 'home.html',
+    //   template: './src/template/home.html',
+    // }),
+    new HtmlWebpackPlugin({
+      //   filename: 'index.html',
+      template: './src/template/blog.html',
+    }),
+    new MiniCssExtractPlugin(),
+  ],
+};
